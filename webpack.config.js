3e2e8f8b0e6d886e47b916a498e4dcfdc7f6da0b
boris blanche <borisblanche77gmail.com>
2024-06-05 17:22:38 +0200

    Projet terminé

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,15 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const PurgeCSSPlugin = require('purgecss-webpack-plugin');
-const glob = require('glob-all');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: './assets/bootstrap/bootstrap.css', // Remplacez par le chemin de votre fichier CSS principal
+  entry: './assets/scripts.js', // Chemin vers votre fichier JS principal
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js', // Un nom de fichier JS est requis pour l'entrée, mais il sera ignoré car nous ne générons que du CSS
+    filename: 'scripts.min.js', // Nom du fichier JS compressé
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'bootstrap.min.css', // Nom du fichier CSS compressé
-    }),
-    new PurgeCSSPlugin({
-      paths: glob.sync([
-        path.join(__dirname, 'Projet9/index.html'),
-        path.join(__dirname, 'Projet9/assets/scripts.js'),
-        path.join(__dirname, 'Projet9/assets/maugallery.bundle.js'),
-        path.join(__dirname, 'Projet9/assets/style.css'),
-      ]),
-    }),
-  ],
   optimization: {
     minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(),
-    ],
+    minimizer: [new TerserWebpackPlugin()],
   },
   mode: 'production',
 };
-
-
-
